521ff9c9922bb79ab60a2c7b4337da701a5d1cc5
mmshafi22 <mmohammedshafi70@gmail.com>
2023-02-15 09:25:46 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import {Switch, Route} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 // Replace your code here
 const App = () => (
   <Switch>
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/" component={Home} />
+    <Route exact path="/ebank/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
     <Route component={NotFound} />
   </Switch>
 )
